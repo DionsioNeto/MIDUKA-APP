d72be5a0ee8f02e1e40f8ca58d46b36ecb25e807
Dionísio <dionisioni304@gmail.com>
2025-03-25 02:21:21 +0100

    migrations de guardados

---
+++ database/migrations/2025_03_25_020151_create_guardados_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('guardados', function (Blueprint $table) {
+            $table->id();
+            //Criando chave estrangeira
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('conteudo_id');
+
+            //relacionando a chave "user_id" com o id da tabela "user"
+            $table->foreign('user_id')
+            ->references('id')
+            ->on('users');
+
+            $table->foreign('conteudo_id')
+            ->references('id')
+            ->on('conteudos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('guardados');
+    }
+};
