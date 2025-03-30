1993f4fbba4caf06b8e8031f82a3fc7593e7b898
Dionísio <dionisioni304@gmail.com>
2025-03-30 18:38:20 +0100

    ..

--- resources/views/livewire/no-content.blade.php
+++ resources/views/livewire/no-content.blade.php
@@ -2,7 +2,5 @@
     <div class="alert-content">
         <i class="fa-solid fa-circle-exclamation"></i>
         <h1>Sem conteúdo algum adicionado</h1>
-        <p>Isto deve-se a falta postagens</p>
-        </p>
     </div>
 </div>
