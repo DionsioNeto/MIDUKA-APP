c1979d7ce609c93f3c3d93fe4a980fc3ed211cc6
Dionísio <dionisioni304@gmail.com>
2025-04-07 03:18:33 +0100

    registando o user_type_admin na model user, garanido a sua existencia

--- app/Models/User.php
+++ app/Models/User.php
@@ -75,6 +75,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'user_type_admin' => 'boolean',
         ];
     }
 }
