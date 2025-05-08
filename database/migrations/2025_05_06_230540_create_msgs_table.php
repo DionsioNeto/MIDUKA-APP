31061e48c3c979d74ae41624af1f849da81c1328
Dionísio <dionisioni304@gmail.com>
2025-05-08 12:46:29 +0100

    Migration de mensagem de support

---
+++ database/migrations/2025_05_06_230540_create_msgs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('msgs', function (Blueprint $table) {
+            $table->id();
+            $table->string('email');
+            $table->string('phoneNumber');
+            $table->string('description');
+            $table->string('typeProblem');
+
+            //Criando chave estrangeira
+            $table->unsignedBigInteger('user_id');
+
+             //relacionando a chave "user_id" com o id da tabela "user"
+             $table->foreign('user_id')
+             ->references('id')
+             ->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('msgs');
+    }
+};
