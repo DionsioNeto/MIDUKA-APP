a556112aa62466156668ad9072f68866c170502d
Dionísio <dionisioni304@gmail.com>
2025-05-16 19:45:57 +0100

    clean code

--- database/migrations/2025_02_05_180045_create_likes_table.php
+++ database/migrations/2025_02_05_180045_create_likes_table.php
@@ -4,36 +4,30 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
+return new class extends Migration {
+    
+    public function up(): void {
         Schema::create('likes', function (Blueprint $table) {
             $table->id();
-             //Criando chave estrangeira
-             $table->unsignedBigInteger('user_id');
-             $table->unsignedBigInteger('conteudo_id');
+            //Criando chave estrangeira
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('conteudo_id');
+            //relacionando a chave "user_id" com o id da tabela "user"
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
 
-             //relacionando a chave "user_id" com o id da tabela "user"
-             $table->foreign('user_id')
-             ->references('id')
-             ->on('users');
-
-             $table->foreign('conteudo_id')
-             ->references('id')
-             ->on('conteudos');
+            $table->foreign('conteudo_id')
+                ->references('id')
+                ->on('conteudos')
+                ->onDelete('cascade');
+                
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('likes');
     }
 };
