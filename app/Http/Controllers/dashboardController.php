0c734733cb1f7a37d4dcdf824d4e0578cd6adb57
Dionísio <dionisioni304@gmail.com>
2025-05-16 19:59:54 +0100

    direcionando para a pagina de conteudos quando um conteudo e deletado

--- app/Http/Controllers/dashboardController.php
+++ app/Http/Controllers/dashboardController.php
@@ -115,7 +115,7 @@ class dashboardController extends Controller{
 
     public function destroy_conteudo($id){
         Conteudo::findOrFail($id)->delete();
-        return redirect("/dashboard-support")->with("delete", "Conteudo Excluído com sucesso.");
+        return redirect("/dashboard-conteudos")->with("delete", "Conteudo Excluído com sucesso.");
     }
 
     public function destroy_user($id){
