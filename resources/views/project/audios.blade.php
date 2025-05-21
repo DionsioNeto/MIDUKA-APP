5657ad8e4210f070f2257e62e0bf2d070f5647d2
Dionísio <dionisioni304@gmail.com>
2025-05-21 23:37:46 +0100

    versao final

--- resources/views/project/audios.blade.php
+++ resources/views/project/audios.blade.php
@@ -1,20 +1,12 @@
 @extends('layouts.principal')
 @section('title', 'Mi | Audios')
 @session('content')
-
 @endsession
 <livewire:header />
 <livewire:sidbar />
 <livewire:sidbar-right />
 <livewire:responsive-nav />
-<main>
-    <livewire:audios />
-    <div wire:offline>
-        <livewire:all-pages />
-    </div>
-</main>
-
-
-
-
-
+<livewire:audios />
+<div wire:offline>
+    <livewire:all-pages />
+</div>
