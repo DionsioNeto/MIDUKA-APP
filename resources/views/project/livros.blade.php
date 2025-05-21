7ac2159b5e872ef724787c0c707f7ea9746dbf66
Dionísio <dionisioni304@gmail.com>
2025-05-21 23:37:10 +0100

    versao final

--- resources/views/project/livros.blade.php
+++ resources/views/project/livros.blade.php
@@ -1,16 +1,12 @@
 @extends('layouts.principal')
 @section('title', 'Mi | Livros & PDFs')
 @session('content')
-
 @endsession
 <livewire:header />
 <livewire:sidbar />
 <livewire:sidbar-right />
 <livewire:responsive-nav />
-<main>
-    <livewire:livros />
-    <div wire:offline>
-        <livewire:all-pages />
-    </div>
-</main>
-
+<livewire:livros />
+<div wire:offline>
+    <livewire:all-pages />
+</div>
