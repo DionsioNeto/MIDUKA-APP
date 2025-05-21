0bc2f5e47f405318530fa6db648068cc2dd66f0f
Dionísio <dionisioni304@gmail.com>
2025-05-21 23:35:42 +0100

    versao final

--- resources/views/project/videos.blade.php
+++ resources/views/project/videos.blade.php
@@ -1,20 +1,12 @@
 @extends('layouts.principal')
 @section('title', 'Mi | Vídeos')
 @session('content')
-
 @endsession
 <livewire:header />
 <livewire:sidbar />
 <livewire:sidbar-right />
 <livewire:responsive-nav />
-<main>
-    <livewire:videos />
-    <div wire:offline>
-        <livewire:all-pages />
-    </div>
-</main>
-
-
-
-
-
+<livewire:videos />
+<div wire:offline>
+    <livewire:all-pages />
+</div>
