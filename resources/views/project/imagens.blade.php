477585f7fe74f43871e995f954abbd72c012c554
Dionísio <dionisioni304@gmail.com>
2025-05-21 23:37:26 +0100

    versao final

--- resources/views/project/imagens.blade.php
+++ resources/views/project/imagens.blade.php
@@ -1,20 +1,12 @@
 @extends('layouts.principal')
 @section('title', 'Mi | Imagens')
 @session('content')
-
 @endsession
 <livewire:header />
 <livewire:sidbar />
 <livewire:sidbar-right />
 <livewire:responsive-nav />
-<main>
-    <livewire:imagens />
-    <div wire:offline>
-        <livewire:all-pages />
-    </div>
-</main>
-
-
-
-
-
+<livewire:imagens />
+<div wire:offline>
+    <livewire:all-pages />
+</div>
