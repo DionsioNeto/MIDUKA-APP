c2b44762f8d6c9f1ba6a4cfbcf4e5f07c82f02c0
Dionísio <dionisioni304@gmail.com>
2025-05-27 12:35:02 +0100

    layout para renderizar livewire

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>MI</title>
+        <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+            integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+            crossorigin="anonymous"
+            referrerpolicy="no-referrer"
+        />        
+        @livewireStyles
+    </head>
+    <body>  
+        <main>
+            {{ $slot }}
+        </main>
+        @livewireScripts
+    </body>
+</html>
