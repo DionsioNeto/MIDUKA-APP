858157e915bdc07d435d60e393963904775e3d22
Dionísio <dionisioni304@gmail.com>
2025-05-27 12:25:30 +0100

    Customizando a rota de visualizacao de conteudo para utilizar com o livewire

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use App\Livewire\{
 // Rota principal da aplicação (index).
 Route::get('/', [indexController:: class, 'index']);
 // Rota para ver um conteúdo
-Route::get('/ver/{id}',  [Ver::class, 'render']);
+Route::get('/ver/{id}', Ver::class);
 //Rota que nos leva a view de perfis de outros usuários...
 
 Route::get('/usuario/{id}',[Usuario::class, 'render']);
