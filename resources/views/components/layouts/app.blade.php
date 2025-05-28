330c2cda8d6cbcbe8ac00496e051de3a483be784
Dionísio <dionisioni304@gmail.com>
2025-05-28 23:31:54 +0100

    adicionando caminho para o js

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,7 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>MI</title>
         <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
@@ -20,5 +19,6 @@
             {{ $slot }}
         </main>
         @livewireScripts
+        <script src="{{ asset('./js/script.js') }}"></script>
     </body>
 </html>
